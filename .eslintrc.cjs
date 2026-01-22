431267a3b6fd3d66ea4ff2420123eefe70511750
lmariagonzalesl <maria.leonor.gonzales@gmail.com>
2026-01-22 17:13:59 -0400

    Frontend (Vue 3 + Vite)

---
+++ .eslintrc.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  root: true,
+  env: { node: true },
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+  ],
+  parserOptions: { ecmaVersion: 'latest' }
+};
